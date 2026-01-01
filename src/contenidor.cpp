15a7bccd32b13ed09c9ffbc2e55a13ebc099334a
guillem.lago <guillem.lago@estudiantat.upc.edu>
2026-01-01 13:25:54 +0100

    Reorganización del repositorio.

--- src/contenidor.cpp
+++ src/contenidor.cpp
@@ -5,6 +5,9 @@
 // extremadamente pequeñas) más rápido y más claro.
 
 // Constructors
+// Pre: -
+// Post: Crea un contenidor amb els valors m i l.
+//		 Altrament, llença una excepció.
 contenidor::contenidor(const string &m, nat l)
 	: _matricula{m}, _longitud{l}
 {
@@ -18,14 +21,20 @@ contenidor::contenidor(const string &m, nat l)
 	}
 
 	// Validació longitud: 10, 20 o 30
-	if( l != 10 && l != 20 && l != 30)
+	if(l != 10 && l != 20 && l != 30)
 		throw error(LongitudIncorrecta);
 }
 
+// Constructor per còpia
+// Pre: Existeix un contenidor u amb uns valors vàlids.
+// Post: Crea un contenidor amb els valors d'u.
 contenidor::contenidor(const contenidor &u)
 	: _matricula{u._matricula}, _longitud{u._longitud}
 {}
 
+// Constructor per assignació
+// Pre: P.I i u són contenidors vàlids.
+// Post: Al P.I s'assignen els valors de matricula i longitud d'u.
 contenidor& contenidor::operator=(const contenidor &u)
 {
     if(this != &u)
@@ -36,46 +45,63 @@ contenidor& contenidor::operator=(const contenidor &u)
     return *this;
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: El P.I es destruit.
 contenidor::~contenidor() noexcept {}
 
 // Consultors
+// Pre: -
+// Post: Retorna la longitud del P.I.
 nat contenidor::longitud() const noexcept
 {
     return _longitud;
 }
 
+// Pre: -
+// Post: Retorna la matricula del P.I.
 string contenidor::matricula() const noexcept
 {
     return _matricula;
 }
 
-//Modificador
+//Operadors
+// Pre: Existeix un P.I vàlid.
+// Post: Retorna si ambdós contenidors (P.I i c) són iguals o no.
 bool contenidor::operator==(const contenidor &c) const noexcept
 {
     return _matricula == c._matricula && _longitud == c._longitud;
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si ambdós contenidors (P.I i c) són diferents o no.
 bool contenidor::operator!=(const contenidor &c) const noexcept
 {
     return !(*this == c);
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és menor que c.
 bool contenidor::operator<(const contenidor &c) const noexcept
 {
-	return _matricula < c._matricula ||
-		(_matricula == c._matricula && _longitud < c._longitud);
+	return _matricula < c._matricula || (_matricula == c._matricula && _longitud < c._longitud);
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és menor o igual que c.
 bool contenidor::operator<=(const contenidor &c) const noexcept
 {
     return !(c < *this);
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és major que c.
 bool contenidor::operator>(const contenidor &c) const noexcept
 {
     return (c < *this);
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és major o igual que c.
 bool contenidor::operator>=(const contenidor &c) const noexcept
 {
     return !(*this < c);
--- src/ubicacio.cpp
+++ src/ubicacio.cpp
@@ -5,11 +5,13 @@
 //accidentals.
 
 // Constructor
+// Pre: -
+// Post: Crea una ubicació <i, j, k>.
+//		 De forma alterna, llença una excepció.
 ubicacio::ubicacio(int i, int j, int k)
 	: _fila{i}, _columna{j}, _altura{k}
 {
-    bool cas_especial = (i == -1 && j == 0 && k == 0) ||
-            (i == -1 && j == -1 && k == -1);
+    bool cas_especial = (i == -1 && j == 0 && k == 0) || (i == -1 && j == -1 && k == -1);
 
     bool cas_normal = (i >= 0 || j >= 0 || k >= 0);
 
@@ -19,17 +21,21 @@ ubicacio::ubicacio(int i, int j, int k)
 }
 
 // Constructor per copia
+// Pre: Existeix una ubicació u amb uns valors <i, j, k> vàlids.
+// Post: Crea una ubicació <i, j, k> amb els valors d'u.
 ubicacio::ubicacio(const ubicacio& u)
 	: _fila{u._fila}, _columna{u._columna}, _altura{u._altura}
 {}
 
 // Constructor per assignació
+// Pre: P.I i u són una ubicacions vàlides.
+// Post: Al P.I s'assigna els valors <i, j, k> d'u.
 ubicacio& ubicacio::operator=(const ubicacio& u)
 {
 	if(this != &u)
 	{
 		_fila = u._fila;
-		_columna=u._columna;
+		_columna= u._columna;
 		_altura = u._altura;
 	}
 
@@ -37,32 +43,42 @@ ubicacio& ubicacio::operator=(const ubicacio& u)
 }
 
 // Destructor
+// Pre: Existeix P.I vàlid.
+// Post: El P.I es destruit.
 ubicacio::~ubicacio() noexcept {}
 
 // Consultors
+// Pre: -
+// Post: Retorna la filera on es troba el P.I.
 int ubicacio::filera() const noexcept
 {
     return _fila;
 }
 
+// Pre: -
+// Post: Retorna la columna on es troba el P.I.
 int ubicacio::placa() const noexcept
 {
     return _columna;
 }
 
+// Pre: - 
+// Post: Retorna l'altura on es troba el P.I.
 int ubicacio::pis() const noexcept
 {
     return _altura;
 }
 
 // Operators
+// Pre: Existeix P.I vàlid. 
+// Post: Retorna si ambdós ubicacions (P.I i u) són iguals o no.
 bool ubicacio::operator==(const ubicacio &u) const noexcept
 {
-	return _fila == u._fila &&
-		_columna == u._columna &&
-		_altura == u._altura;
+	return _fila == u._fila && _columna == u._columna && _altura == u._altura;
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si ambdós ubicacions (P.I i u) són diferents o no.
 bool ubicacio::operator!=(const ubicacio &u) const noexcept
 {
 	return !(*this == u);
@@ -71,24 +87,29 @@ bool ubicacio::operator!=(const ubicacio &u) const noexcept
 //Implementació prioritzant performance (sense comparacions
 //redundants) i eficient a nivell de cache (Accessos a
 //memoria sequencials)
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és menor que u. 
 bool ubicacio::operator<(const ubicacio &u) const noexcept
 {
-	return _fila < u._fila ||
-		(_fila == u._fila && (_columna < u._columna ||
-				      (_columna == u._columna &&
-				       _altura < u._altura)));
+	return _fila < u._fila || (_fila == u._fila && (_columna < u._columna || (_columna == u._columna && _altura < u._altura)));
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és menor o igual que u. 
 bool ubicacio::operator<=(const ubicacio &u) const noexcept
 {
 	return !(u < *this);
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és major que u. 
 bool ubicacio::operator>(const ubicacio &u) const noexcept
 {
 	return (u < *this);
 }
 
+// Pre: Existeix P.I vàlid.
+// Post: Retorna si P.I és major o igual que u. 
 bool ubicacio::operator>=(const ubicacio &u) const noexcept
 {
 	return !(*this < u);
